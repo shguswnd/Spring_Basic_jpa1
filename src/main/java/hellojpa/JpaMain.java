96dfc8edfa2b180b43b6831f42512c8a82bb957b
nhj <nhj6336@gmail.com>
2024-01-08 23:09:12 +0900

    240108

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -76,6 +76,29 @@ public class JpaMain {
             em.flush();
 */
 
+            Team team = new Team();
+            team.setName("TeamA");
+            em.persist(team);
+
+            Member member = new Member();
+            member.setUsername("member1");
+            member.setTeam(team);
+//            member.setTeamId(team.getId());
+            em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+
+//            Long findTeamId = findMember.getTeamId();
+//            Team findTeam = em.find(Team.class, findTeamId);
+//            Team findTeam = findMember.getTeam();
+            List<Member> members = findMember.getTeam().getMembers();
+
+            for (Member m : members) {
+                System.out.println("m = " + m.getUsername());
+            }
             tx.commit();
 
         } catch (Exception e){
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -8,14 +8,19 @@ import java.util.Date;
 @SequenceGenerator(name = "member_seq_generator", sequenceName = "member_seq")
 public class Member {
 
-    @Id
-    @GeneratedValue(strategy =  GenerationType.SEQUENCE, generator = "member_seq_generator")
+/*    @GeneratedValue(strategy =  GenerationType.SEQUENCE, generator = "member_seq_generator")*/
+    @Id @GeneratedValue
+    @Column(name = "MEMBER_ID")
     private Long id;
-    @Column(name = "name")
+    @Column(name = "USERNAME")
     private String username;
 
-    public Member(){
-    }
+//    @Column(name = "TEAM_ID")
+//    private Long teamId;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
 
     public Long getId() {
         return id;
@@ -31,5 +36,13 @@ public class Member {
 
     public void setUsername(String username) {
         this.username = username;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 }
